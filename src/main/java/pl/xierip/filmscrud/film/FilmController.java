5649a37d1f6225de11fb89ab570d9206922663f0
Patryk Bernacki <patrykbernacki@outlook.com>
2019-12-07 17:27:04 +0100

    Facade calls from controller

--- src/main/java/pl/xierip/filmscrud/film/FilmController.java
+++ src/main/java/pl/xierip/filmscrud/film/FilmController.java
@@ -1,14 +1,21 @@
 package pl.xierip.filmscrud.film;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.xierip.filmscrud.film.domain.FilmFacade;
+import pl.xierip.filmscrud.film.domain.dto.FilmCreationDto;
 import pl.xierip.filmscrud.film.domain.dto.FilmDto;
 import pl.xierip.filmscrud.film.domain.dto.FilmDataUpdateDto;
 import pl.xierip.filmscrud.infrastructure.constant.ApiLayers;
@@ -17,29 +24,31 @@ import pl.xierip.filmscrud.infrastructure.constant.ApiLayers;
 @RequestMapping(ApiLayers.FILMS)
 class FilmController {
 
+  private FilmFacade filmFacade;
+
   @PostMapping
-  FilmDto createFilm() {
-    return null;
+  ResponseEntity<FilmDto> createFilm(FilmCreationDto filmCreationDto) {
+    return ResponseEntity.ok(filmFacade.add(filmCreationDto));
   }
 
   @GetMapping
-  Page<FilmDto> fetchFilms() {
-    return null;
+  ResponseEntity<Page<FilmDto>> fetchFilms(PageRequest pageRequest) {
+    return ResponseEntity.ok(filmFacade.showAll(pageRequest));
   }
 
   @GetMapping("{id}")
-  Page<FilmDto> fetchFilmById(@PathVariable ObjectId id) {
-    return null;
+  ResponseEntity<FilmDto> fetchFilmById(@PathVariable @Valid @NotNull ObjectId id) {
+    return ResponseEntity.ok(filmFacade.show(id));
   }
 
   @PutMapping("{id}")
-  Page<FilmDto> updateFilmData(FilmDataUpdateDto filmDto) {
-    return null;
+  ResponseEntity<Page<FilmDto>> updateFilmData(@RequestBody @Valid @NotNull FilmDataUpdateDto filmDataUpdateDto) {
+    return ResponseEntity.ok(filmFacade.update(filmDataUpdateDto));
   }
 
   @DeleteMapping("{id}")
-  Page<FilmDto> deleteFilm(@PathVariable ObjectId id) {
-    return null;
+  ResponseEntity< Page<FilmDto>> deleteFilm(@PathVariable ObjectId id) {
+    return ResponseEntity.ok(filmFacade.remove(id));
   }
 
 
--- src/main/java/pl/xierip/filmscrud/film/domain/FilmFacade.java
+++ src/main/java/pl/xierip/filmscrud/film/domain/FilmFacade.java
@@ -1,5 +1,31 @@
 package pl.xierip.filmscrud.film.domain;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import pl.xierip.filmscrud.film.domain.dto.FilmCreationDto;
+import pl.xierip.filmscrud.film.domain.dto.FilmDataUpdateDto;
+import pl.xierip.filmscrud.film.domain.dto.FilmDto;
+
 public class FilmFacade {
 
+  public FilmDto add(FilmCreationDto filmCreationDto) {
+    return null;
+  }
+
+  public Page<FilmDto> showAll(PageRequest pageRequest) {
+    return null;
+  }
+
+  public FilmDto show(ObjectId id) {
+    return null;
+  }
+
+  public Page<FilmDto> update(FilmDataUpdateDto filmDataUpdateDto) {
+    return null;
+  }
+
+  public FilmDto remove(ObjectId id) {
+    return null;
+  }
 }
